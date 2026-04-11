014c175501083094dd5ca543353218bdb11d9692
kasperukwiktor-code <kasperukwiktor@gmail.com>
2026-04-11 12:42:29 +0200

    feat: add basic data validation and column normalization
    feat: add basic data validation and column normalization

--- etl/extract_stock_data.py
+++ etl/extract_stock_data.py
@@ -2,24 +2,29 @@ import yfinance as yf
 import pandas as pd
 from datetime import datetime
 
-TICKERS = ["TSLA", "AAPL", "MSFT", "PLTR", "AMD", "LMT","AVGO"]
+TICKERS = ["TSLA", "AAPL", "MSFT", "PLTR", "AMD", "LMT", "AVGO"]
 
 START_DATE = "2021-01-01"
 END_DATE = "2025-01-01"
 
-
 def extract_data(ticker):
     df = yf.download(ticker, start=START_DATE, end=END_DATE)
+    df.columns = df.columns.get_level_values(0)  # ← spłaszcza MultiIndex
     df.reset_index(inplace=True)
     df["ticker"] = ticker
     return df
 
 
 def transform_data(df):
-    df["daily_return"] = df["Close"].pct_change()
-    df["moving_avg_7d"] = df["Close"].rolling(window=7).mean()
-    df["year"] = df["Date"].dt.year
-    df["month"] = df["Date"].dt.month
+    if df.empty:
+        print("Warning: empty dataframe")
+        return df
+    df = df.dropna()
+    df.columns = [col.lower() for col in df.columns]
+    df["daily_return"] = df["close"].pct_change()
+    df["moving_avg_7d"] = df["close"].rolling(window=7).mean()
+    df["year"] = df["date"].dt.year
+    df["month"] = df["date"].dt.month
     return df
 
 
